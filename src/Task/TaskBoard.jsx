cb0dabdc0f61c18693af975514811dd23af0557c
Sharoar Hossain <maffin.killer007@gmail.com>
2025-11-12 16:58:00 +0600

    refactor: remove search task from taskboard and create separate component and use it in taskboard

--- src/Task/TaskBoard.jsx
+++ src/Task/TaskBoard.jsx
@@ -1,43 +1,12 @@
+import SearchTask from "./SearchTask";
+
 export default function TaskBoard() {
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
         {/* <!-- Search Box --> */}
         <div className="p-2 flex justify-end">
-          <form>
-            <div className="flex">
-              <div className="relative overflow-hidden rounded-lg text-gray-50 md:min-w-[380px] lg:min-w-[440px]">
-                <input
-                  type="search"
-                  id="search-dropdown"
-                  className="z-20 block w-full bg-gray-800 px-4 py-2 pr-10 focus:outline-none"
-                  placeholder="Search Task"
-                  required
-                />
-                <button
-                  type="submit"
-                  className="absolute right-2 top-0 h-full rounded-e-lg text-white md:right-4"
-                >
-                  <svg
-                    className="h-4 w-4"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                    />
-                  </svg>
-                  <span className="sr-only">Search</span>
-                </button>
-              </div>
-            </div>
-          </form>
+          <SearchTask />
         </div>
         {/* <!-- Search Box Ends --> */}
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
@@ -52,6 +21,7 @@ export default function TaskBoard() {
               </button>
             </div>
           </div>
+
           <div className="overflow-auto">
             <table className="table-fixed overflow-auto xl:w-full">
               <thead>
