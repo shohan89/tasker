362c44f9ea4152b7092200643b002af31e33d64e
Sharoar Hossain <maffin.killer007@gmail.com>
2025-11-12 17:01:38 +0600

    refactor: remove task actions and create separate component

--- src/Task/TaskBoard.jsx
+++ src/Task/TaskBoard.jsx
@@ -1,4 +1,5 @@
 import SearchTask from "./SearchTask";
+import TaskActions from "./TaskActions";
 
 export default function TaskBoard() {
   return (
@@ -10,18 +11,8 @@ export default function TaskBoard() {
         </div>
         {/* <!-- Search Box Ends --> */}
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <div className="mb-14 items-center justify-between sm:flex">
-            <h2 className="text-2xl font-semibold max-sm:mb-4">Your Tasks</h2>
-            <div className="flex items-center space-x-5">
-              <button className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold">
-                Add Task
-              </button>
-              <button className="rounded-md bg-red-500 px-3.5 py-2.5 text-sm font-semibold">
-                Delete All
-              </button>
-            </div>
-          </div>
-
+          {/* task action */}
+          <TaskActions />
           <div className="overflow-auto">
             <table className="table-fixed overflow-auto xl:w-full">
               <thead>
