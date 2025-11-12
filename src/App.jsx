0df036b951313d79e549ff59b49722bf59b35310
Sharoar Hossain <maffin.killer007@gmail.com>
2025-11-12 16:50:53 +0600

    feat: add Task Board section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Footer from "./Footer"
 import Header from "./Header"
 import HeroSection from "./HeroSection"
+import TaskBoard from "./Task/TaskBoard"
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header />
       <HeroSection />
+      <TaskBoard />
       <Footer />
     </>
   )
